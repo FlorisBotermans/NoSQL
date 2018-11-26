15ac14bfbec6a3073db60252c050a172c1b01e68
abouhazz <ayman_elb112@hotmail.com>
2018-11-26 16:01:05 +0100

    Thread controllers

--- controllers/threads_controller.js
+++ controllers/threads_controller.js
@@ -1,5 +1,11 @@
 const Thread = require('../models/thread');
 
 module.exports = {
-    
+    createnew(req, res, next){
+        const thread = req.body;
+
+        Thread.create(thread)
+        .then(user => res.send(thread))
+        .catch(next);
+    }
 };
