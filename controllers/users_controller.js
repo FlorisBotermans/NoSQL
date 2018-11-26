3c0421fabe394ca2837f0100584fd9c793452e11
abouhazz <ayman_elb112@hotmail.com>
2018-11-26 14:12:08 +0100

    create, edit and delete function created for user controller

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -7,5 +7,24 @@ module.exports = {
         User.create(user)
             .then(user => res.send(user))
             .catch(next);
+    },
+
+    editUser(req, res, next){
+        const userBody = req.body;
+        const userId = req.params.id;
+
+        User.edit({_id:userId}, userBody)
+        .then(()=>User.findById({_id:userId}))
+        .then(user => res.send(user))
+        .catch(next);
+    },
+
+    deleteUser(req, res, next){
+        const userId = req.params.id;
+
+        User.findByIdAndRemove({_id:userId})
+        .then(user => res.status(204).send(user))
+        .catch(next);
+
     }
 }
