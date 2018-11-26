9ee29a790a06f1d926d81992c6e09a32e0d98247
abouhazz <ayman_elb112@hotmail.com>
2018-11-26 13:22:04 +0100

    Crud routes for users and threads created

--- routes/routes.js
+++ routes/routes.js
@@ -5,8 +5,17 @@ const CommentsController = require('../controllers/comments_controller');
 module.exports = (app) => {
     // user CRUD
     app.post('/api/users', UsersController.createNewUser);
+    app.get('/api/users', UsersController.getAllUser);
+    app.put('/api/users/:userid', UsersController.editUser);
+    app.get('/api/users/:userid', UsersController.getUserById);
+    app.delete('/api/users/:userid', UsersController.deleteUser);
 
     // thread CRUD
+    app.post('/api/users/:userid/threads', ThreadsController.createNewThread);
+    app.get('/api/users/:userid/threads', ThreadsController.getThreads);
+    app.put('/api/users/userid/:userid/threads/:threadid', ThreadsController.editThread);
+    app.get('/api/users/userid/:userid/threads/:threadid', ThreadsController.getThreadById);
+    app.delete('/api/users/userid/:userid/threads/:threadid', ThreadsController.deleteThread);
 
     // comment CRUD
 }
