bf13ad396c02e7268e8511dbf11bbdf9af604dfa
abouhazz <ayman_elb112@hotmail.com>
2018-11-29 14:40:27 +0100

    ffff

--- test/controllers/friendships_controller_test.js
+++ test/controllers/friendships_controller_test.js
@@ -38,4 +38,38 @@ describe('Friendships controller', ()=>{
                 done(err)
             })
     });
+
+    it('DELETE to api/friendship that deletes a friendship', done => {
+        const name1 = 'jan';
+        const name2 = 'piet';
+        let session = driver.session();
+    
+        session.run(
+            'MATCH (a:User),(b:User) WHERE a.userName = $userName1 AND b.userName = $userName2 CREATE (a)-[r:IS_FRIENDS_WITH]->(b) RETURN type(r)',
+            {
+                userName1 : name1,
+                userName2: name2
+            })
+            .then(()=> {
+                return session.run('MATCH (a:User),(b:User) WHERE a.userName = $userName1 AND b.userName = $userName2 CREATE (a)-[r:IS_FRIENDS_WITH]->(b) RETURN type(r)',
+                {
+                    userName1 : name1,
+                    userName2: name2
+                })
+            })
+            .then((result) => {
+                const single = result.records[0];
+                const node = single.get(0);
+                assert(
+                    name1 === node.properties.userName1,
+                    name2 === node.properties.userName2
+                    );
+                session.close();
+                done();
+            })
+            .catch((err)=>{
+                session.close();
+                done(err)
+            })
+    });
 });
