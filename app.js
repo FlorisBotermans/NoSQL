f95d447cf6372645c590a1ff4f36e7d0d6e6d589
Floris Botermans <f.botermans@student.avans.nl>
2018-11-29 16:44:51 +0100

    fdsaffd

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cool = require('cool-ascii-faces');
 const mongoose = require('mongoose');
 const routes = require('./routes/routes');
+const path = require('path');
 const app = express();
 
 mongoose.Promise = global.Promise;
@@ -11,6 +13,9 @@ mongoose.set('useFindAndModify', false);
 if (process.env.NODE_ENV !== 'test') {
     mongoose.connect('mongodb://admin:Secret123@ds044979.mlab.com:44979/studdit', { useNewUrlParser: true });
 }
+
+app.use(express.static(path.join(__dirname, 'public')))
+    .get('/cool', (req, res) => res.send(cool()));
 
 app.use(bodyParser.json());
 routes(app);
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const app = require('./app');
+const PORT = process.env.PORT || 5000;
 
-app.listen(3050, () => {
-    console.log('Running on port 3050');
+app.listen(PORT, () => {
+    console.log(`Running on port ${ PORT }`);
 });
